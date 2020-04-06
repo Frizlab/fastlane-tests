ce15b0f2d12aed76430895f55a668b68f2542f1b
Frizlab <fload@me.com>
2020-04-06 17:25:09 +0200

    Initial Commit

---
+++ fastlane-tests/ContentView.swift
@@ -0,0 +1,24 @@
+/*
+ * ContentView.swift
+ * fastlane-tests
+ *
+ * Created by François Lamboley on 06/04/2020.
+ * Copyright © 2020 Frost Land. All rights reserved.
+ */
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
